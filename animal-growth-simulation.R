5cfccd2839f672fb4719a5c3c3ebdd9f15c6d63f
seyederfan-hosseinihaji <seyederfan.hosseinihaji@gmail.com>
2025-09-06 20:50:44 +0330

    Add: R script to calculate FCR and final cattle weight after 30 days

--- animal-growth-simulation.R
+++ animal-growth-simulation.R
@@ -1,1 +1,22 @@
-InitialWeight <-
+#Insert Initial fattening weight here(Kg):
+InitialFatteningWeight <- 220
+#insert Average Daily Gain (ADG) here(Kg):
+DailyGain <- 1.2
+#Weight Gain After 30 Days
+TotalWeightGain <- 30 * DailyGain
+# Calculate final weight after 30 days based on initial fattening weight and ADG
+B <- InitialFatteningWeight + TotalWeightGain
+
+
+#Total feed intake(Kg):
+TotalFeedIntake <- 240
+#Feed Conversion Ratio (FCR)
+FCR <- round(TotalFeedIntake / TotalWeightGain, 2)
+
+
+#Final result
+sentence <- paste(
+  "After 30 days, the animal weighs", B, "kg",
+  "with an FCR of", FCR
+)
+sentence
